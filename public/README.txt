c841f2f9ac7ba3d227338d64083e06920369896c
a-rothwell <root@DESKTOP-NFU7HTT.localdomain>
2018-04-13 00:30:08 -0400

    fixed auth

--- public/README.txt
+++ public/README.txt
@@ -1,3 +1,11 @@
 Lime:
 
 Online budgeting app
+
+Heidi Anderson : ander788@purdue.edu
+Andrew Rothwell : arothwel@purdue.edu
+
+Technologies:
+MDBootstrap
+Firebase
+Google Maps
--- public/js/script.js
+++ public/js/script.js
@@ -9,25 +9,77 @@
       };
       firebase.initializeApp(config);
 
-var newAccountButton = document.getElementById('newAccount');
-var email = document.getElementById('emailField');
-var password = document.getElementById('passwordField');
-var nameInput = document.getElementById('nameField');
-var phone = document.getElementById('phoneField');
+      var newAccountButton = document.getElementById('newAccount');
+      var email = document.getElementById('emailField');
+      var password = document.getElementById('passwordField');
+      var nameInput = document.getElementById('nameField');
+      var phone = document.getElementById('phoneField');
+      var loginEmail = document.getElementById('loginemail');
+      var loginPassword = document.getElementById('loginpassword');
+      var loginButton = document.getElementById('loginButton');
+      var logoutButton = document.getElementById('logout');
 
-newAccountButton.onclick = function(){
-    console.log("Function");
-    console.log(email.value);
-    console.log(password.value);
-    console.log(nameInput.value);
-    console.log(phone.value);
-    firebase.auth().createUserWithEmailAndPassword(email.value, password.value).catch(function(error) {
-        // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        // ...
-      }).then(() => {
-        window.location = "home.html";
+
+      firebase.auth().onAuthStateChanged(function (user) {
+        console.log(user);
+        if (!user && !(window.location.pathname == '/index.html' || window.location.pathname == '/pswd.html' || window.location.pathname == '/newacc.html')) {
+          window.location = '/index.html';
+        }
       });
-}
+      if (logoutButton != null) {
+        logoutButton.onclick = function () {
+          firebase.auth().signOut().then(function () {
+            // Sign-out successful.
+          }).catch(function (error) {
+            // An error happened.
+          });
+        }
+      }
 
+      if (loginButton != null) {
+        loginButton.onclick = function () {
+          console.log("Function");
+          console.log(loginEmail.value);
+          console.log(loginPassword.value);
+          firebase.auth().signInWithEmailAndPassword(loginEmail.value, loginPassword.value).then(() => {
+              window.location = "home.html";
+            })
+            .catch(function (error) {
+              // Handle Errors here.
+              var errorCode = error.code;
+              var errorMessage = error.message;
+
+              console.log(errorMessage);
+              // ...
+            });
+        }
+      }
+
+      if (newAccountButton != null) {
+        console.log("Here");
+        newAccountButton.onclick = function () {
+          firebase.auth().createUserWithEmailAndPassword(email.value, password.value).then(() => {
+            var uid = firebase.auth().currentUser.uid;
+            console.log(uid);
+
+            var userObject = {
+              uid : uid,
+              phone: phone.value,
+              name : nameInput.value
+          }
+          var updates = {};
+          updates['/UserInfo/' + uid] = userObject;
+          return firebase.database().ref().update(updates);
+          })
+          .then(() => {
+            window.location = "home.html";
+          })
+          .catch(function (error) {
+            // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            // ...
+            console.log(errorMessage);
+          });
+        }
+      }
