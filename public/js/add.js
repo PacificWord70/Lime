9244693af0f2dfffaa2d331a965bfe08b1808377
a-rothwell <a_rothwell@outlook.com>
2018-04-14 20:49:08 -0400

    Add selector and budget change support

--- public/js/add.js
+++ public/js/add.js
@@ -1,4 +1,92 @@
-function myFunction(){
+var budgetSelect = document.getElementById('budgetSelect');
+var catogoriesSelect = document.getElementById('catogoriesSelect');
+
+
+
+function myFunction() {
     document.getElementById("moneyamt");
     document.getElementById("streetaddr");
 }
+
+$(document).ready(function () {
+    $('.js-example-basic-single').select2();
+    $('.js-example-basic-multiple').select2();
+});
+
+$("#budgetSelect").on('change', function () {
+    console.log(budgetSelect.value)
+    var user = firebase.auth().currentUser;
+    return firebase.database().ref('/UserInfo/' + user.uid + '/UserBudgets/').once('value').then(function (snapshot) {
+        var reads = [];
+        snapshot.forEach(function (childSnapshot) {
+            var id = childSnapshot.key;
+            console.log(id);
+            var promise = firebase.database().ref('/Budgets/' + id).once('value').then(function (snap) {
+                return snap.val();
+                // The Promise was fulfilled.
+            }, function (error) {
+                console.error(error);
+            });
+            console.log(promise);
+            reads.push(promise);
+        });
+        return Promise.all(reads);
+    }, function (error) {
+        console.error(error);
+    }).then(function (values) {
+        console.log(values);
+        var i = 0;
+        for (var bug of values) {
+            if(bug.name == budgetSelect.value) {
+                break;
+            }
+            i++;
+        }
+        console.log(values[i]);
+        var str1 = ''
+        for (var cat in values[i].categories) {
+            console.log(cat)
+            str1 = str1 + "<option value=\"" + cat + "\">" + cat + "</option>"
+        }
+
+        catogoriesSelect.innerHTML = str1;
+    });
+});
+
+firebase.auth().onAuthStateChanged(function (user) {
+    return firebase.database().ref('/UserInfo/' + user.uid + '/UserBudgets/').once('value').then(function (snapshot) {
+        var reads = [];
+        snapshot.forEach(function (childSnapshot) {
+            var id = childSnapshot.key;
+            console.log(id);
+            var promise = firebase.database().ref('/Budgets/' + id).once('value').then(function (snap) {
+                return snap.val();
+                // The Promise was fulfilled.
+            }, function (error) {
+                console.error(error);
+            });
+            console.log(promise);
+            reads.push(promise);
+        });
+        return Promise.all(reads);
+    }, function (error) {
+        console.error(error);
+    }).then(function (values) {
+        console.log(values);
+        var str = ''
+        for (var bug of values) {
+            str = str + "<option value=\"" + bug.name + "\">" + bug.name + "</option>"
+        }
+        console.log(values[0]);
+        var str1 = ''
+        for (var cat in values[0].categories) {
+            console.log(cat)
+            // console.log(bug.categories[cat])
+            str1 = str1 + "<option value=\"" + cat + "\">" + cat + "</option>"
+        }
+
+        console.log(str);
+        budgetSelect.innerHTML = str;
+        catogoriesSelect.innerHTML = str1;
+    });
+});
