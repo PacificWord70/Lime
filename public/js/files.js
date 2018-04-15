ffed8565066dd5b4683535fd5b15afe287407d70
a-rothwell <a_rothwell@outlook.com>
2018-04-14 22:37:41 -0400

    Update

---
+++ public/js/files.js
@@ -0,0 +1,40 @@
+var allexpenses = document.getElementById('allexpenses');
+
+firebase.auth().onAuthStateChanged(function (user) {
+    // return firebase.database().ref('/UserInfo/' + user.uid + '/UserBudgets/').once('value').then(function (snapshot) {
+    //     var reads = [];
+    //     snapshot.forEach(function (childSnapshot) {
+    //         var id = childSnapshot.key;
+    //         console.log(id);
+    //         var promise = firebase.database().ref('/Budgets/' + id).once('value').then(function (snap) {
+    //             return snap.val();
+    //             // The Promise was fulfilled.
+    //         }, function (error) {
+    //             console.error(error);
+    //         });
+    //         console.log(promise);
+    //         reads.push(promise);
+    //     });
+    //     return Promise.all(reads);
+    // }, function (error) {
+    //     console.error(error);
+    // }).then(function (values) {
+    //     console.log(values);
+    //     var str = ''
+    //     for (var bug of values) {
+    //         str = str + "<option value=\"" + bug.BID + "\">" + bug.name + "</option>"
+    //     }
+    //     console.log(values[0]);
+    //     var str1 = ''
+    //     for (var cat in values[0].categories) {
+    //         console.log(cat)
+    //         // console.log(bug.categories[cat])
+    //         str1 = str1 + "<option value=\"" + cat + "\">" + cat + "</option>"
+    //     }
+
+    //     console.log(str);
+    //     budgetSelect.innerHTML = str;
+    //     catogoriesSelect.innerHTML = str1;
+    // });
+    allexpenses.innerHTML = "You spend the money"
+});
--- public/js/script.js
+++ public/js/script.js
@@ -57,52 +57,6 @@
               profilePhonenumber.value = snapshot.val().phone;
               profileEmail.value = user.email;
             });
-          } else if (window.location.pathname == '/home.html') {
-            // console.log("Here");
-            // return firebase.database().ref('/UserInfo/' + user.uid + '/UserBudgets/').once('value').then(function (snapshot) {
-            //   var reads = [];
-            //   snapshot.forEach(function (childSnapshot) {
-            //     var id = childSnapshot.key;
-            //     console.log(id);
-            //     var promise = firebase.database().ref('/Budgets/' + id).once('value').then(function (snap) {
-            //       return snap.val();
-            //       // The Promise was fulfilled.
-            //     }, function (error) {
-            //       console.error(error);
-            //     });
-            //     console.log(promise);
-            //     reads.push(promise);
-            //   });
-            //   return Promise.all(reads);
-            // }, function (error) {
-            //   console.error(error);
-            // }).then(function (values) {
-            //   console.log(values);
-            //   var str = ''
-            //   for(var bug of values){
-            //     str = str + "<h1>" + bug.name + "</h1>"
-            //     str = str + "<table>"
-            //               + "<tr>"
-            //               + "<th>Catagory</th>"
-            //               + "<th>Amount Set</th>"
-            //               + "</tr>"
-            //     for(var cat in bug.categories){
-            //       str = str
-            //               + "<tr>"
-            //               + "<th>" + cat + "</th>"
-            //               + "<th>" + bug.categories[cat] + "</th>"
-            //               + "</tr>"
-            //       console.log(cat)
-            //       console.log(bug.categories[cat])
-            //     }
-            //     console.log(bug.categories)
-            //     console.log(bug.name)
-            //     str = str + "</table>"
-            //   }
-              
-            //   console.log(str);
-            //   budgetSummary.innerHTML = str;
-            // });
           }
         }
       });
