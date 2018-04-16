a3510c403de4600082f61e9df155d7ebd3687c72
a-rothwell <a_rothwell@outlook.com>
2018-04-16 15:05:52 -0400

    Add robots.txt

--- public/js/map.js
+++ public/js/map.js
@@ -16,7 +16,7 @@ firebase.auth().onAuthStateChanged(function (user) {
   var infoWindow = new google.maps.InfoWindow;
   map = new google.maps.Map(document.getElementById('map'), {
     center: home,
-    zoom: 14,
+    zoom: 10,
     mapTypeId: 'satellite'
   });
 
