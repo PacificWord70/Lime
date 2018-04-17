b33a73dc56be8bd1f7812cb8fc6837badc2a77c6
a-rothwell <a_rothwell@outlook.com>
2018-04-17 14:31:27 -0400

    Share Working

--- public/js/share.js
+++ public/js/share.js
@@ -8,28 +8,28 @@ if(user == null){
 console.log(window.location.href)
 var url = new URL(window.location.href);
 console.log(url.searchParams.get("budget"));
-
+console.log("Changing text");
 status.innerHTML = "Adding Budget to your budget"
 
-// jQuery.ajax({
-//     url: "https://us-central1-lime-4e46e.cloudfunctions.net/message/share?UID=" + user.uid + "&budget=" + url.searchParams.get("budget") ,
-//     type: "GET",
-//     data: {
-//     },
-//     dataType: "json",
-//     beforeSend: function (x) {
-//       if (x && x.overrideMimeType) {
-//         x.overrideMimeType("application/j-son;charset=UTF-8");
-//       }
-//     },
-//     success: function (result) {
-//       //Write your code here
-//       console.log("Success",result);
+jQuery.ajax({
+    url: "https://us-central1-lime-4e46e.cloudfunctions.net/message/share?UID=" + user.uid + "&budget=" + url.searchParams.get("budget") ,
+    type: "GET",
+    data: {
+    },
+    dataType: "json",
+    beforeSend: function (x) {
+      if (x && x.overrideMimeType) {
+        x.overrideMimeType("application/j-son;charset=UTF-8");
+      }
+    },
+    success: function (result) {
+      //Write your code here
+      console.log("Success",result);
       
-//     },
-//     complete: function(data) {
-//         // window.location = "home.html";
-//     }
-//   });
+    },
+    complete: function(data) {
+        // window.location = "home.html";
+    }
+  });
 
 });
