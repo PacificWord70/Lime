05c1532c6e5c2fc92308db2abdb0dee726541def
PacificWord70 <heidianderson@sfhs.com>
2018-04-23 21:19:23 -0400

    moving around graphs

--- public/js/map.js
+++ public/js/map.js
@@ -16,7 +16,7 @@ firebase.auth().onAuthStateChanged(function (user) {
   var infoWindow = new google.maps.InfoWindow;
   map = new google.maps.Map(document.getElementById('map'), {
     center: home,
-    zoom: 10,
+    zoom: 11,
     mapTypeId: 'satellite'
   });
 
